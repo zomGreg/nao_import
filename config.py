62927e8dce42c9d85a1349f9231e1ce631f3b75c
zomgreg <greg.moselle@enstratius.com>
2015-04-22 08:45:42 -0500

    fixing mysql import

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'mysql://root:trustno1@127.0.0.1:3306/nao'
+
--- nethack.py
+++ nethack.py
@@ -1,86 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import urllib2
 import string
-
-from sqlalchemy import create_engine, schema, types
-from BeautifulSoup import BeautifulSoup
-import prettytable
-from collections import Counter
-import pickle, simplejson
 import datetime
-from os import listdir
-from os.path import isfile, join
-
-import create_db
-
-#players=['bose','mscurmudgeon','stth','Vodalus','muad']
-players = ['zomgreg']
-
-
-def get_player_file(player_name):
-    '''
-    This function will save the associated player_name html file
-    into the ./player_html directory.
-    '''
-
-    #player_name="zomgreg"
-
-    req_string = 'http://alt.org/nethack/player-all-xlog.php?player=%s' % (player_name)
-
-    req = urllib2.Request(req_string)
-    response = urllib2.urlopen(req)
-    html = response.read()
-    ##
-    f = open('./player_files/%s.html' % player_name, 'w')
-    f.write(html)
-    f.close()
-
-g = create_db.Games()
-
-metadata = schema.MetaData()
-engine = create_engine('sqlite:///players.db', echo=False)
-metadata.bind = engine
-
-def process_html_file(player_file):
-
-    f = open(player_file, 'r')
-    soup = BeautifulSoup(f)
-    all_games = soup.findAll('pre')[0].string.split('\n')
-    print "Importing %s games." % (len(all_games))
-    f.close()
-
-    game_list = []
-    print len(all_games)
-    for i in range(0, len(all_games) - 1):
-        keys = [t.split('=')[0] for t in all_games[i].split(':')]
-        values = [t.split('=')[1] for t in all_games[i].split(':')]
-        game = dict(zip(keys, values))
-        game_list.append(game)
-
-    for game in game_list:
-        game_dict = dict(align0=game.get('align0', 'None'), deathlev=game['deathlev'], uid=game['uid'],
-                         deaths=game['deaths'],
-                         turns=game.get('turns', 0), points=game['points'], death=game['death'],
-                         realtime=game.get('realtime', 0),
-                         version=game['version'], role=game['role'], conduct=game.get('conduct', 'None'),
-                         gender0=game.get('gender0', 'N/A'),
-                         deathdate=datetime.datetime.strptime(game['deathdate'], "%Y%m%d").date(), hp=game['hp'],
-                         achieve=game.get('achieve', 0), gamedelta=game.get('gamedelta', 0), maxlvl=game['maxlvl'],
-                         maxhp=game['maxhp'],
-                         endtime=datetime.datetime.fromtimestamp(int(game.get('endtime', 0))),
-                         nachieves=game.get('nachieves', 0),
-                         nconducts=game.get('nconducts', 0), name=game['name'], gender=game['gender'],
-                         align=game['align'],
-                         birthdate=datetime.datetime.strptime(game['birthdate'], "%Y%m%d").date(), race=game['race'],
-                         flags=game.get('flags', 0),
-                         starttime=datetime.datetime.fromtimestamp(int(game.get('starttime', 0))),
-                         deathdnum=game['deathdnum']
-        )
-
-        create_db.load_game(game_dict)
-
 
 def process_player_file(player_file):
     f = open(player_file, 'r')
@@ -89,35 +11,85 @@ def process_player_file(player_file):
         keys = [t.split('=')[0] for t in line.split(':')]
         values = [filter(lambda x: x in string.printable, t.split('=')[1]) for t in line.split(':')]
 
-        #for v in values:
-        #	print v, filter(lambda x: x in string.printable, v),type(v)
-
         game = dict(zip(keys, values))
 
-        game_dict = dict(align0=game.get('align0', 'None'), deathlev=game['deathlev'], uid=game['uid'],
+        if game.get('conduct') is not None:
+            conduct = int(game['conduct'], 0)
+        else:
+            conduct = 0
+
+        if game.get('achieve') is not None:
+            achieve = int(game['achieve'], 0)
+        else:
+            achieve = 0
+
+        if game.get('flags') is not None:
+            flags = int(game['flags'], 0)
+        else:
+            flags = 0
+
+        if game.get('align0') is not None:
+            align0 = game['align0'].rstrip()
+        else:
+            align0 = game.get('align0')
+
+        game_dict = dict(align0=align0, deathlev=game['deathlev'], uid=game['uid'],
                          deaths=game['deaths'],
-                         turns=game.get('turns', 0), points=game['points'], death=game['death'],
+                         turns=game.get('turns', 0), points=int(game['points']), death=game['death'],
                          realtime=game.get('realtime', 0),
-                         version=game['version'], role=game['role'], conduct=game.get('conduct', 'None'),
+                         version=game['version'], role=game['role'], conduct=conduct,
                          gender0=game.get('gender0', 'N/A'),
                          deathdate=datetime.datetime.strptime(game['deathdate'], "%Y%m%d").date(), hp=game['hp'],
-                         achieve=game.get('achieve', 0), gamedelta=game.get('gamedelta', 0), maxlvl=game['maxlvl'],
+                         achieve=achieve, gamedelta=game.get('gamedelta', 0), maxlvl=game['maxlvl'],
                          maxhp=game['maxhp'],
                          endtime=datetime.datetime.fromtimestamp(int(game.get('endtime', 0))),
                          nachieves=game.get('nachieves', 0),
                          nconducts=game.get('nconducts', 0), name=game['name'], gender=game['gender'],
                          align=game['align'],
                          birthdate=datetime.datetime.strptime(game['birthdate'], "%Y%m%d").date(), race=game['race'],
-                         flags=game.get('flags', 0),
+                         flags=flags,
                          starttime=datetime.datetime.fromtimestamp(int(game.get('starttime', 0))),
                          deathdnum=game['deathdnum']
-        )
+                         )
 
-        create_db.load_game(game_dict)
+        nao_game = models.Game(align0=game_dict['align0'],
+                               deathlev=game_dict['deathlev'],
+                               uid=game_dict['uid'],
+                               deaths=game_dict['deaths'],
+                               turns=game_dict['turns'],
+                               realtime=game_dict['realtime'],
+                               points=game_dict['points'],
+                               death=game_dict['death'],
+                               version=game_dict['version'],
+                               role=game_dict['role'],
+                               conduct=game_dict['conduct'],
+                               gender0=game_dict['gender0'],
+                               deathdate=game_dict['deathdate'],
+                               hp=game_dict['hp'],
+                               achieve=game_dict['achieve'],
+                               gamedelta=game_dict['gamedelta'],
+                               maxlvl=game_dict['maxlvl'],
+                               maxhp=game_dict['maxhp'],
+                               endtime=game_dict['endtime'],
+                               nachieves=game_dict['nachieves'],
+                               nconducts=game_dict['nconducts'],
+                               name=game_dict['name'],
+                               gender=game_dict['gender'],
+                               align=game_dict['align'],
+                               birthdate=game_dict['birthdate'],
+                               race=game_dict['race'],
+                               flags=game_dict['flags'],
+                               starttime=game_dict['starttime'],
+                               deathdnum=game_dict['deathdnum']
+                               )
+        # print nao_game.align0
+        db.session.add(nao_game)
+        db.session.commit()
+        # keys = [k for k in game_dict.iterkeys()]
+        # print len(keys)
+        # print game_dict
+        # time.sleep(1)
 
 
-process_html_file('./player_files/stth.html')
-
-#process_player_file('/home/dcm/first10000.txt')
-#process_player_file('/home/dcm/xlogfile.full.txt')
-#process_player_file('/home/dcm/next.txt')
+# process_player_file('/Users/gmoselle/xlogfile.full.txt.new.new.new')
+process_player_file('/Users/gmoselle/xlogfile.full.txt')
